8e37a36fd2ae2038de8170a102f3a3539907035c
Olga Panfilova <gr-olga@ya.ru>
2022-04-19 18:13:20 +0200

    playground with JSON

--- playground/cool.js
+++
@@ -1,1 +0,0 @@
-{"title":"form","author":"god"}
--- playground/jiso.js
+++ playground/jiso.js
@@ -1,13 +1,21 @@
-import {writeFile, readFile} from 'fs'
-const book = {
-    title: "form",
-    author: 'god'
-}
+import {writeFile, readFile}  from 'fs'
+import * as fs from "fs";
+// const book = {
+//     title: "form",
+//     author: 'god'
+// }
 
-const bookJSON = JSON.stringify(book)
-writeFile('cool.js',bookJSON,() => console.log('file is saved'))
+// const bookJSON = JSON.stringify(book)
+// writeFile('cool.json',bookJSON,() => console.log('file is saved'))
 
-readFile('cool.js', (err, data) => {
-    if (err) throw  err;
-    console.log(JSON.parse(data.toString()).title)
-})
+const buffer = fs.readFileSync('cool.json')
+console.log(buffer);
+const dataJSON = buffer.toString()
+console.log(dataJSON)
+const user = JSON.parse(dataJSON)
+user.name = "Moo"
+user.age = '99'
+console.log(user)
+
+const userJSON = JSON.stringify(user)
+writeFile('cool.json', userJSON,() => console.log('file is saved'))
